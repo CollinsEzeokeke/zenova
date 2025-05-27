58dacddbdd278d2a3bb7c40ca9110a8e60946b8a
Collins Ezeokeke <collinsmecosy@gmail.com>
2025-05-27 07:26:42 +0100

    Add @mastra/deployer-vercel dependency and integrate VercelDeployer in Mastra initialization for deployment configuration

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,13 +1,15 @@
-
 import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
-
-
+import { VercelDeployer} from '@mastra/deployer-vercel';
 import { zenovaAgent } from './agents/ZenovaAgent';
 
 export const mastra = new Mastra({
+  deployer: new VercelDeployer({
+    projectName: process.env.NEXT_PUBLIC_VERCEL_PROJECT_ID as string,
+    teamSlug: process.env.NEXT_PUBLIC_VERCEL_TEAM_ID as string,
+    token: process.env.NEXT_PUBLIC_VERCEL_TOKEN as string,
+  }),
   agents: { zenovaAgent },
- 
   logger: new PinoLogger({
     name: 'Mastra',
     level: 'info',
